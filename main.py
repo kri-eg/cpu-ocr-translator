7a6b8951c907160a7ef3ad14f6582e1bd16f37f6
minsky-lab <shehul.singh05@gmail.com>
2025-07-29 02:17:51 +0530

    refactor: Update .gitignore to exclude build files

--- main.py
+++
@@ -1,9 +0,0 @@
-# File: main.py
-
-from src.gui.main_window import App
-
-if __name__ == "__main__":
-    # This is the official entry point of our application.
-    # It creates an instance of our main window class and starts the GUI event loop.
-    app = App()
-    app.mainloop()
